c153da3a9a3a8f3a9bba4a3b5e3165ce16ab0f3f
Daniel Figueiredo <docarmorama@gmail.com>
2025-06-17 20:08:43 -0300

    Aula 53 - Forma não controlada para o input

--- chronos-pomodoro/src/componentes/MainForm/index.tsx
+++ chronos-pomodoro/src/componentes/MainForm/index.tsx
@@ -2,14 +2,14 @@ import { Cycles } from '../Cycles';
 import { PlayCircleIcon } from 'lucide-react';
 import { DefaultInput } from '../DefaultInput';
 import { DefaultButton } from '../DefaultButton';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Deu certo', taskName);
+    console.log('Deu certo', taskNameInput.current?.value);
   }
 
   return (
@@ -21,9 +21,7 @@ export function MainForm() {
           id='meuInput'
           type='text'
           placeholder='Digite algo'
-          value={taskName}
-          // onChange={e => {console.log('Event: ', e.target.value);}}
-          onChange={e => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
       <div className='formRow'>
