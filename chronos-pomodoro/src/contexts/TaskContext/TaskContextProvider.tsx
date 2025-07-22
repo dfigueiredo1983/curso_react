2c4ab0c52e2e0a45b592b8ddd03f13c8be3a9dd7
DISIS-Ibram <gesis.ibram@gmail.com>
2025-07-22 14:27:15 -0300

    Aula 67

--- chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,8 +1,6 @@
-import { useReducer, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
-import { taskReducer } from './taskReducer';
-import { TaskActionTypes } from './taskActions';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -11,12 +9,8 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  // const(estado_inicial, dispatch) = useReducer(função_reducer, estado_inicial)
-  const [myState, dispatch] = useReducer(taskReducer, initialTaskState);
-
   useEffect(() => {
     console.log(state);
-    dispatch({ type: TaskActionTypes.RESET_STATE });
   }, [state]);
 
   return (
